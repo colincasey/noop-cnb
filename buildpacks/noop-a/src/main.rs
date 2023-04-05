e1fb9a7b4cba586976a6fa6f97318c042d1e1dd4
Colin Casey <casey.colin@gmail.com>
2023-04-05 11:04:15 -0300

    Test project for automation

---
+++ buildpacks/noop-a/src/main.rs
@@ -0,0 +1,21 @@
+use libcnb::build::{BuildContext, BuildResult, BuildResultBuilder};
+use libcnb::{Buildpack, buildpack_main};
+use libcnb::detect::{DetectContext, DetectResult, DetectResultBuilder};
+use libcnb::generic::{GenericError, GenericMetadata, GenericPlatform};
+
+impl Buildpack for NoopBuildpackA {
+    type Platform = GenericPlatform;
+    type Metadata = GenericMetadata;
+    type Error = GenericError;
+
+    fn detect(&self, _context: DetectContext<Self>) -> libcnb::Result<DetectResult, Self::Error> {
+        DetectResultBuilder::fail().build()
+    }
+
+    fn build(&self, _context: BuildContext<Self>) -> libcnb::Result<BuildResult, Self::Error> {
+        BuildResultBuilder::new().build()
+    }
+}
+
+
+buildpack_main!(NoopBuildpackA);
